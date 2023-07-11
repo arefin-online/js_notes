f822e248cc0dcae81e52069c04104e7a76ed5f57
Md. Morshedul Arefin <arefin2k@gmail.com>
2023-07-12 01:00:04 +0600

    initial

---
+++ math.js
@@ -0,0 +1,62 @@
+// JavaScript provides a variety of built -in math methods that you can use to perform mathematical operations on numbers.
+
+// ✅ abs()
+// var num = -7;
+// var result = Math.abs(num);
+// console.log(result); // 7
+
+
+// ✅ ceil()
+// var num = 7.2;
+// var result = Math.ceil(num);
+// console.log(result); // 8
+
+
+// ✅ floor()
+// var num = 7.2;
+// var result = Math.floor(num);
+// console.log(result); // 7
+
+
+// ✅ max()
+// var result = Math.max(22,34,45,12,32);
+// console.log(result); // 45
+
+
+// ✅ min()
+// var result = Math.min(22,34,45,12,32);
+// console.log(result); // 12
+
+
+// ✅ random() - it will generate random numbers between 0 to 1
+// var result = Math.random();
+// console.log(result); // 0.10142289022634299
+
+
+// ✅ round()
+// var result = Math.round(12.34);
+// console.log(result); // 12
+
+// var result = Math.round(12.78);
+// console.log(result); // 13
+
+
+// ✅ PI
+// console.log(Math.PI); // 3.141592653589793
+
+
+// ✅ sqrt()
+// var result = Math.sqrt(144);
+// console.log(result); // 12
+
+
+// ✅ pow()
+// var result = Math.pow(2,3);
+// console.log(result); // 8
+
+
+
+
+
+
+
---
+++ number.js
@@ -0,0 +1,46 @@
+// ✅ isFinite()
+// var result = Number.isFinite(1234);
+// console.log(result); // true
+
+
+// ✅ isInteger()
+// var result = Number.isInteger(7.5);
+// console.log(result); // false
+
+
+// ✅ parseInt()
+// var result = Number.parseInt(7.5);
+// console.log(result); // 7
+
+
+// ✅ toFixed()
+// var num = 7.578747584365;
+// var result = num.toFixed(3);
+// console.log(result); // 7.579
+
+// var num = 7.578747584365;
+// var result = num.toFixed(0);
+// console.log(result); // 8
+
+
+// ✅ toString()
+// var num = 7.57;
+// var result = num.toString();
+// console.log(result); // 7.57
+
+
+// ✅ MAX_VALUE
+// console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
+
+
+// ✅ MIN_VALUE
+// console.log(Number.MIN_VALUE); // 5e-324
+
+
+// ✅ isNaN()
+// var a = 12;
+// console.log(Number.isNaN(a));  // false
+
+
+
+
