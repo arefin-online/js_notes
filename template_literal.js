f83023cffa0ccd924f333a51d23f0212e577eb2a
Md. Morshedul Arefin <arefin2k@gmail.com>
2023-07-11 16:30:13 +0600

    initial

---
+++ template_literal.js
@@ -0,0 +1,21 @@
+// Template Literals or Template Strings, provide a convenient way to concatenate strings and include dynamic expressions within them. They were introduced in ECMAScript 6(ES6) and have become widely adopted due to their readability and flexibility.
+
+// ✅ Without Template Literal
+// const fname = "Morshedul"
+// const lname = "Arefin"
+// const result = "My full name is " + fname + " " + lname
+// console.log(result)
+
+// ✅ Using Template Literal - Example 1
+// const fname = "Morshedul"
+// const lname = "Arefin"
+// const result = `My full name is ${fname} ${lname}`
+// console.log(result)
+
+// ✅ Using Template Literal - Example 2
+// function fullName() {
+//     return "Morshedul Arefin"
+// }
+// const result = `My name is ${fullName()}`
+// console.log(result)
+
